6e1a226e10e996ce581713fc2186bdde84acaa3a
20141105048 <l20141105048@King.local>
2016-07-11 15:22:30 +0800

    第二次提交
    将表更新为user，然后添加上邮箱与用户名选项。

--- hangge_645/ViewController.swift
+++ hangge_645/ViewController.swift
@@ -7,13 +7,15 @@ class ViewController: UIViewController {
     @IBOutlet var txtUname: UITextField!
     @IBOutlet var txtMobile: UITextField!
     
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var address: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //获取数据库实例
         db = SQLiteDB.sharedInstance()
         //如果表还不存在则创建表
-        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        db.execute("create table if not exists user(uid integer primary key,uname varchar(20),mobile varchar(20),email varchar(20),address varchar(20))")
         //如果有数据则加载
         initUser()
     }
@@ -25,12 +27,14 @@ class ViewController: UIViewController {
     
     //从SQLite加载数据
     func initUser() {
-        let data = db.query("select * from t_user")
+        let data = db.query("select * from user")
         if data.count > 0 {
             //获取最后一行数据显示
             let user = data[data.count - 1]
             txtUname.text = user["uname"] as? String
             txtMobile.text = user["mobile"] as? String
+            email.text = user["email"] as? String
+            address.text = user["address"] as? String
         }
     }
     override func didReceiveMemoryWarning() {
@@ -41,8 +45,10 @@ class ViewController: UIViewController {
     func saveUser() {
         let uname = self.txtUname.text!
         let mobile = self.txtMobile.text!
+        let email = self.email.text!
+        let address = self.address.text!
         //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
-        let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+        let sql = "insert into user(uname,mobile,email,address) values('\(uname)','\(mobile)','\(email)','\(address)')"
         print("sql: \(sql)")
         //通过封装的方法执行sql
         let result = db.execute(sql)
--- hangge_645/find.swift
+++ hangge_645/find.swift
@@ -13,7 +13,7 @@ class find: UIViewController {
     
     
     @IBAction func qingkong(sender: AnyObject) {
-        let sql = "delete from t_user"
+        let sql = "delete from user"
         let result = db.execute(sql)
         print(result)
     }
@@ -28,7 +28,7 @@ class find: UIViewController {
         //获取数据库实例
         db = SQLiteDB.sharedInstance()
         //如果表还不存在则创建表（其中uid为自增主键）
-        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20)")
+        db.execute("create table if not exists user(uid integer primary key,uname varchar(20),mobile varchar(20),email varchar(20),address varchar(20))")
         //如果有数据则加载
         // initUser()
     }
@@ -40,13 +40,13 @@ class find: UIViewController {
     func selete(){
         text.text=""
         
-        let data = db.query("select * from t_user")
+        let data = db.query("select * from user")
         
         for var x=0;x<data.count;x++
         {
             //获取最后一行数据显示
             let tuser = data[x]
-            text.text! += "姓名：" + String(tuser["uname"]!) + " 电话：" + String(tuser["mobile"]!) 
+            text.text! += "姓名：" + String(tuser["uname"]!) + " 电话：" + String(tuser["mobile"]!) + " 邮箱：" + String(tuser["email"]!) + " 地址：" + String(tuser["address"]!)
         }
         
     }
